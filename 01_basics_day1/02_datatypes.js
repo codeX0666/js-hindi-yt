5a6830be6f8556638839d9b573ef13f02e2cd7c4
codeX0666 <ajitxak479136@gmail.com>
2024-07-14 08:55:06 +0000

    datatpes

---
+++ 01_basics_day1/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // it treat all JS code in newer version//
+
+ //alert  ( 3 + 3 ) we  are using  nodejs , not browser //
+
+ console.log(3 + 3); console.log("ajit"); // ase too ye code run huga but isami readability nhi hai // 
+
+console.log( 3 + 3 );
+console.log("AJIT"); // but readability  ( jo  code ko acche se smajhne or padne mai  madat kre )isko bolte hai //
+
+
+let name = "ajit";
+let age = 21; 
+let isLoggedIn = false
+
+
+// number => 2 to the power 53   //
+// bade number ke liye  "bigint" //
+// string => ""                  // 
+// boolean => true/ false        //
+// null =>  standalone value ,empty object ( matlab khali hai )  //
+// undefined => iska matlab  VARIABLE DECLARED HAI BUT VALUE ASSIGN NHI KI HAI //
+// symbol => unique // 
+// object  //
+
+console.log(typeof "ajit"); //  iska datatype string //
+ 
+console.log(typeof 21 ); // iska datatype number //
+
+console.log(typeof null);  // iska datatype 'object' //
+
+console.log(typeof undefined); // iska datatype ' undefined ' // 
+
