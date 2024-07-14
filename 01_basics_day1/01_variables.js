98316cca63e167cbdd5c630c9ae8f7d14e5d2ec7
codeX0666 <ajitxak479136@gmail.com>
2024-07-14 08:54:21 +0000

    variables

---
+++ 01_basics_day1/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 544414
+let accountEmail = "codex@gmail.com"
+var accountCity = "mumbai"
+let accountState ;
+
+
+  //  accountID = 2 //  
+
+ accountEmail = "gamex@gmail.com"
+ accountCity = "hyderabad"
+ 
+
+console. table([accountID, accountEmail, accountCity, accountState ]);
+
+/* prefer not use of var bcoz od block of scope */
